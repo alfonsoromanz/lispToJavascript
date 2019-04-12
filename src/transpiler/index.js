3d0baf91cd383945b2537070ca062ad74d23b6fb
Alfonso Roman Zubeldia <alfonsoromanz24@gmail.com>
2019-04-12 17:19:47 -0300

    Added a Parser and a simplified grammar to test variable declarations and expressions #3 #4

--- src/transpiler/index.js
+++ src/transpiler/index.js
@@ -6,6 +6,8 @@
  * @author Alfonso
  */
 const Lexer = require('lex');
+const Parser = require("jison").Parser;
+const Grammar = require('./grammar')
 
 module.exports = async function transpileLispToJavascript (hook)  {
   const lispCode = hook.data.lispCode;
@@ -14,10 +16,12 @@ module.exports = async function transpileLispToJavascript (hook)  {
   let lexemes = [];
   let errors = [];
   let line = 1;
-  const lexer = new Lexer;
   
-  //TO-DO: define tokens and give them a numerical ID instead of a string
+  const parser = new Parser(Grammar);
+  const lexer = parser.lexer = new Lexer;
+  
   lexer.addRule(/ /, function (lexeme) {
+    this.yytext = lexeme;
     lexemes.push(lexeme);
     return "BLANK";
   });
@@ -27,78 +31,93 @@ module.exports = async function transpileLispToJavascript (hook)  {
   }, []);
   
   lexer.addRule(/[0-9]/, function (lexeme) {
+    this.yytext = lexeme;
     lexemes.push(lexeme);
     return "CONST_INT";
   });
   
   lexer.addRule(/or/, function (lexeme) {
+    this.yytext = lexeme;
     lexemes.push(lexeme);
     return "OR"
   });
   
   lexer.addRule(/and/, function (lexeme) {
+    this.yytext = lexeme;
     lexemes.push(lexeme);
     return "AND"
   });
   
   lexer.addRule(/not/, function (lexeme) {
+    this.yytext = lexeme;
     lexemes.push(lexeme);
     return "NOT"
   });
   
   lexer.addRule(/nil/, function (lexeme) {
+    this.yytext = lexeme;
     lexemes.push(lexeme);
     return "NIL"
   });
   
   lexer.addRule(/defvar/, function (lexeme) {
+    this.yytext = lexeme;
     lexemes.push(lexeme);
     return "DEFVAR"
   });
   
   lexer.addRule(/let/, function (lexeme) {
+    this.yytext = lexeme;
     lexemes.push(lexeme);
     return "LET"
   });
   
   lexer.addRule(/[a-z]*/, function (lexeme) {
     if (lexeme!=="") {
+      this.yytext = lexeme;
       lexemes.push(lexeme);
       return "IDENTIFIER";
     }  
   });
   
   lexer.addRule(/\(/, function (lexeme) {
+    this.yytext = lexeme;
     lexemes.push(lexeme);
     return "PARENTHESIS_OPEN"
   });
   
   lexer.addRule(/\)/, function (lexeme) {
+    this.yytext = lexeme;
     lexemes.push(lexeme);
     return "PARENTHESIS_CLOSE"
   });
   
   lexer.addRule(/\+/, function (lexeme) {
+    this.yytext = lexeme;
     lexemes.push(lexeme);
     return "PLUS"
   });
   
   lexer.addRule(/\-/, function (lexeme) {
+    this.yytext = lexeme;
     lexemes.push(lexeme);
     return "MINUS"
   });
   
   lexer.addRule(/\*/, function (lexeme) {
+    this.yytext = lexeme;
     lexemes.push(lexeme);
     return "MULT"
   });
   
   lexer.addRule(/\//, function (lexeme) {
+    this.yytext = lexeme;
     lexemes.push(lexeme);
     return "DIV"
   });
   
   lexer.addRule(/\=/, function (lexeme) {
+    this.yytext = lexeme;
     lexemes.push(lexeme);
     return "EQUALS"
   });
@@ -107,22 +126,12 @@ module.exports = async function transpileLispToJavascript (hook)  {
     return "EOF";
   });
   
-  //Read and tokenize the input
-  lexer.input = lispCode;
-  try {
-    while (token!==undefined) {
-      token = lexer.lex();
-      if (token)
-        tokens.push(token);  
-    } 
-  } catch (e) {
-    errors.push('Unexpected token at line number ' + line + '.');
-  }
+  const javascript = parser.parse(lispCode);
   
   hook.result = {
     success: errors.length===0,
-    javascript: "TO-DO", 
     lisp: lispCode,
+    javascript: javascript, 
     tokens: tokens,
     lexemes: lexemes,
     errors: errors
