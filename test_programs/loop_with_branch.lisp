cb0973d0e96baaa99b017c5877c40117d9aa3b8c
Alfonso Roman Zubeldia <alfonsoromanz24@gmail.com>
2019-04-19 08:53:28 -0300

    Fixed test program #9

--- test_programs/loop_with_branch.lisp
+++ test_programs/loop_with_branch.lisp
@@ -1,2 +1,2 @@
 (defvar count 10)
-(loop (if (> count 1) (setq count (- count 1)) (progn (setq count (- count 1))(print "last iteration"))) (when (> count 0) (return a)))
+(loop (if (> count 1) (setq count (- count 1)) (progn (setq count (- count 1))(print "last iteration"))) (when (> count 0) (return count)))
